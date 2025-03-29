d16aaae58b11ee074ed9fcd5b03db4110597ac65
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-03-29 22:18:52 +0530

    UPDATE NAMES

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl font-semibold text-blue-700">N.K Enterprises</h4>
+            <h4 className="text-3xl font-semibold text-blue-700">AYUVENGER BIOTECH</h4>
             <h5 className="text-lg mt-0 mb-2 text-blue-600">
             Delivery within 4-7 working days.
             </h5>
